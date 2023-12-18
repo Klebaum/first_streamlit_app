84b399e09aef09023ef19939b52cb1af64e2db9f
Kleber Rodrigues <52042810+Klebaum@users.noreply.github.com>
2023-12-18 18:56:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ st.header("What fruit would like to add?")
 
 fruit_add = st.text_input('What fruit would you like information about?')
 my_cur.execute("INSERT INTO fruit_load_list values ('{fruit_add}')")
-my_data_rows = my_cur.fethone()
+my_data_rows = my_cur.fetchone()
 st.write('Ty for adding ', fruit_add)
